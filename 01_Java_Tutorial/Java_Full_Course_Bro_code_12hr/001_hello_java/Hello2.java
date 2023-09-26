19be054a3139d28e8f603f58295fe9cce814bb00
YoungHa Kim <ytok1108@aol.com>
2023-09-27 06:50:34 +0900

    Java_Turotial

---
+++ 01_Java_Tutorial/Java_Full_Course_Bro_code_12hr/001_hello_java/Hello2.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\"I love pizza\"");
+		System.out.println("It's really good!");
+	}
+
+}
+
+
+
+
+// "I love pizza"
+// It's really good!
---
+++ 01_Java_Tutorial/Java_Full_Course_Bro_code_12hr/001_hello_java/README.md
@@ -0,0 +1,20 @@
+# Run th Program
+
+```
+
+$ javac Hello.java
+
+
+$ ls
+
+Hello.class Hello.java
+
+
+
+$ java Hello
+
+Hello World! Java Lang
+
+```
+
+
