f48c755c2f8a46e91830720e1c580a985854121c
YoungHa Kim <ytok1108@aol.com>
2023-09-29 21:23:07 +0900

    make 성공

---
+++ java_sample/failed_make_code/make_sample_java02/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+  public static void main(String[] args) {
+    int max = Integer.MAX_VALUE;
+    int min = Integer.MIN_VALUE;
+
+    System.out.println("The maximum value for an int = " + max);
+    System.out.println("The minimum value for an int = " + min);
+
+    byte maxByteValue = Byte.MAX_VALUE;
+    byte minByteValue = Byte.MIN_VALUE;
+
+    System.out.println("The maximum value for an byte = " + maxByteValue);
+    System.out.println("The minimum value for an byte = " + minByteValue);
+  }
+}
