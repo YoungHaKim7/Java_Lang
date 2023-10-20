35ee541cb99380a9f19d57d04da6516d7e271b1d
GYoung <ytok1108@gmail.com>
2023-10-20 11:36:03 +0900

    java vs rust int

---
+++ 04_Rust_for_Java_dev_tutorial/Jermermy_Chone_Rust_for_Java_Dev/p3_int/Java_lang/src/Main.java
@@ -0,0 +1,7 @@
+public class Main { 
+    public static void main(String[] args) { 
+        int x = 123;
+        x +=1;
+        System.out.println("Hello World. Java" + x); 
+    } 
+} 
---
+++ 04_Rust_for_Java_dev_tutorial/Jermermy_Chone_Rust_for_Java_Dev/p3_int/rust_int/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let mut x: i32 = 123;
+    x += 1;
+    println!("Hello world!! {x}");
+}
