564de7eb032003aa4bbb4c220d863454361166bb
GYoung <ytok1108@gmail.com>
2023-10-20 12:57:06 +0900

    java vs rust fn

---
+++ 04_Rust_for_Java_dev_tutorial/Jermermy_Chone_Rust_for_Java_Dev/p4_fn_hello/java_lang/src/Main.java
@@ -0,0 +1,11 @@
+public class Main { 
+    public static void main(String[] args) { 
+        Main.sayHello();
+    } 
+
+    public static void sayHello() {
+        var x = 123.9;
+        x += 1;
+        System.out.println("Hello world!!!" + x);
+    }
+} 
